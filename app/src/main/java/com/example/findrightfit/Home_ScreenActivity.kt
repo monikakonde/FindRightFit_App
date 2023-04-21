8ab6026d23cdf2b4cc260b86aface26b56bb76bf
Monika <monikakonde41@gmail.com>
2023-04-21 13:59:42 +0530

    FindRightFit constraint layout

---
+++ app/src/main/java/com/example/findrightfit/Home_ScreenActivity.kt
@@ -0,0 +1,11 @@
+package com.example.findrightfit
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Home_ScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home_screen)
+    }
+}
---
+++ app/src/main/java/com/example/findrightfit/MarketingSocialMediaActivity.kt
@@ -0,0 +1,11 @@
+package com.example.findrightfit
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MarketingSocialMediaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_marketing_social_media)
+    }
+}
